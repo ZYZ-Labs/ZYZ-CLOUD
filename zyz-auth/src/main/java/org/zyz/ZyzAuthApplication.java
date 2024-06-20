5955afa687f4aaf0894b321a1a452eb14c012175
zhengsf <1773766682@qq.com>
2024-06-20 13:45:59 +0800

    项目初始化

---
+++ zyz-auth/src/main/java/org/zyz/ZyzAuthApplication.java
@@ -0,0 +1,13 @@
+package org.zyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyzAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyzAuthApplication.class, args);
+    }
+
+}
---
+++ zyz-auth/src/test/java/org/zyz/ZyzAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyzAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
