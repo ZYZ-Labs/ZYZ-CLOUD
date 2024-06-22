78fd9561871e1b55928adb01c76cb318e1915355
SilverIceKey <z516798599@gmail.com>
2024-06-22 12:19:25 +0800

    修改包名增加模块层

--- zyz-auth/src/main/java/org/zyz/auth/ZyzAuthApplication.java
+++ zyz-auth/src/main/java/org/zyz/auth/ZyzAuthApplication.java
@@ -1,4 +1,4 @@
-package org.zyz;
+package org.zyz.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- zyz-auth/src/test/java/org/zyz/auth/ZyzAuthApplicationTests.java
+++ zyz-auth/src/test/java/org/zyz/auth/ZyzAuthApplicationTests.java
@@ -1,4 +1,4 @@
-package org.zyz;
+package org.zyz.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
