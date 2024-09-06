f58726740123982ebfa5344433c9464fde014e5d
SilverIceKey <z516798599@gmail.com>
2024-09-06 17:50:47 +0800

    处理security解决consul注册之后因为没有登录强制跳转登录的问题

--- zyz-core/src/main/java/org/zyz/core/util/PasswordUtil.java
+++ zyz-core/src/main/java/org/zyz/core/util/PasswordUtil.java
@@ -1,19 +1,19 @@
 package org.zyz.core.util;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class PasswordUtil {
 
-    private static final PasswordEncoder encoder = new BCryptPasswordEncoder();
-
-    // 加密密码
-    public static String encrypt(String rawPassword) {
-        return encoder.encode(rawPassword);
-    }
-
-    // 验证密码是否匹配
-    public static boolean matches(String rawPassword, String encodedPassword) {
-        return encoder.matches(rawPassword, encodedPassword);
-    }
+//    private static final PasswordEncoder encoder = new BCryptPasswordEncoder();
+//
+//    // 加密密码
+//    public static String encrypt(String rawPassword) {
+//        return encoder.encode(rawPassword);
+//    }
+//
+//    // 验证密码是否匹配
+//    public static boolean matches(String rawPassword, String encodedPassword) {
+//        return encoder.matches(rawPassword, encodedPassword);
+//    }
 }
