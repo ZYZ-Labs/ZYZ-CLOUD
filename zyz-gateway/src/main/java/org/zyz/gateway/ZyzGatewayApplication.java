689bd1a27f8e0c55c91dcd1812de652a6227087b
SilverIceKey <z516798599@gmail.com>
2024-09-09 09:31:31 +0800

    新增gateway

---
+++ zyz-gateway/src/main/java/org/zyz/gateway/ZyzGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.zyz.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyzGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyzGatewayApplication.class, args);
+    }
+
+}
---
+++ zyz-gateway/src/test/java/org/zyz/gateway/ZyzGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zyz.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyzGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
