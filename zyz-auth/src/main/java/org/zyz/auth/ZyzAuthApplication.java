667b8f0b9da4f0bbe65e58e889c366ee0998571b
SilverIceKey <z516798599@gmail.com>
2024-09-09 18:32:34 +0800

    半集成dubbo，然后创建common-interface库

--- zyz-auth/src/main/java/org/zyz/auth/ZyzAuthApplication.java
+++ zyz-auth/src/main/java/org/zyz/auth/ZyzAuthApplication.java
@@ -1,11 +1,13 @@
 package org.zyz.auth;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableDubbo
 public class ZyzAuthApplication {
 
     public static void main(String[] args) {
--- zyz-user/src/main/java/org/zyz/user/ZyzUserApplication.java
+++ zyz-user/src/main/java/org/zyz/user/ZyzUserApplication.java
@@ -1,12 +1,13 @@
 package org.zyz.user;
 
-import org.mybatis.spring.annotation.MapperScan;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableDubbo
 public class ZyzUserApplication {
 
     public static void main(String[] args) {
